84fd9750e560df1df5ce2977adeee2d92b8db254
Vijay Chendrayan Chandrasekar <vijaychendrayan@gmail.com>
2023-03-29 19:59:52 +0530

    Game Screen Design

--- App.js
+++ App.js
@@ -1,13 +1,42 @@
-import { StyleSheet, } from 'react-native';
-
+import { useState } from 'react';
+import { StyleSheet, ImageBackground, SafeAreaView} from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 import StartGameScreen from './screens/StartGameScreen';
+import GameScreen from './screens/GameScreen';
+import Colors from './constants/colors';
 
 export default function App() {
+  const [userNumber, setUserNumber] = useState();
+
+  function pickedNumberHandler(pickedNumber){
+    setUserNumber(pickedNumber);
+  }
+  let screen = <StartGameScreen onPickNumber={pickedNumberHandler}/>;
+  if(userNumber){
+    screen = <GameScreen />
+  }
   return (
-    <StartGameScreen />
+    <LinearGradient colors={[Colors.primary700,Colors.accent500]} style={styles.rootScreen}>
+      <ImageBackground 
+        source={require('./assets/images/dice.jpg')}
+        resizeMode="cover" 
+        style={styles.rootScreen}
+        imageStyle={styles.backgroundImage}
+      >
+      <SafeAreaView style={styles.rootScreen}>
+        {screen}
+      </SafeAreaView>  
+      </ImageBackground>
+    </LinearGradient>    
   );
 }
 
 const styles = StyleSheet.create({
-  container: {},
+  rootScreen: {
+    flex: 1,
+    
+  },
+  backgroundImage:{
+    opacity: 0.15
+  }
 });
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,32 +1,100 @@
-import { StyleSheet, } from 'react-native';
+import { useState } from 'react';
+import { Alert, StyleSheet, } from 'react-native';
 import { TextInput, View } from 'react-native'
-import PrimaryButton from '../components/PrimaryButton';
-function StartGameScreen(){
+import PrimaryButton from '../components/ui/PrimaryButton';
+import Colors from '../constants/colors';
+
+function StartGameScreen({onPickNumber}){
+    const [enteredNumber, setEnteredNumber] = useState();
+
+    function numberInputHandler(enteredText){
+        setEnteredNumber(enteredText);
+    }
+
+    function resetInputHandler(){
+        setEnteredNumber('');
+    }
+
+    function confirmInputHandler(){
+        const chosenNumber = parseInt(enteredNumber);
+        if (isNaN(chosenNumber) || chosenNumber <=0 || chosenNumber > 99){
+            Alert.alert(    
+                'Invalid Number !',
+                'Number has to be between 1 and 99',
+                [{text: 'Okay', style: 'destructive', onPress:resetInputHandler}]
+            )
+           return;
+        }
+        onPickNumber(chosenNumber);
+    }
+
     return(
         <View style={styles.inputContainer}>
-            <TextInput />
-            <PrimaryButton>Reset</PrimaryButton>
-            <PrimaryButton>Confirm</PrimaryButton>
+            <TextInput 
+                style={styles.numberInput} 
+                maxLength={2} 
+                keyboardType="number-pad" 
+                autoCapitalize='none' 
+                autoCorrect={false} 
+                onChangeText={numberInputHandler}
+                value={enteredNumber}
+            />
+            <View style={styles.buttonsContainer}>
+                <View style={styles.buttonContainer}> 
+                    <PrimaryButton 
+                        onPress={resetInputHandler}
+                    >
+                        Reset
+                    </PrimaryButton>
+                </View>
+                <View style={styles.buttonContainer}>
+                    <PrimaryButton 
+                        onPress={confirmInputHandler}
+                    >
+                        Confirm
+                    </PrimaryButton>
+                </View>
+            </View>
         </View>
     );
-
 }
 
 export default StartGameScreen;
 
 const styles = StyleSheet.create({
     inputContainer:{
-        
+        justifyContent: 'center',
+        alignItems: 'center',
         padding: 16,
         marginTop: 100,
         marginHorizontal: 24,
         borderRadius: 8,
-        backgroundColor: '#72063c',
+        backgroundColor: Colors.primary800,
         elevation: 4,  // android
         shadowColor: 'black',  // iOS
         shadowOffset: { width:0, height: 2}, //iOS
         shadowRadius: 6,  // iOS
         shadowOpacity: 0.25  // iOS
 
+    },
+    numberInput:{
+        height: 50,
+        width: 50,
+        fontSize: 32,
+        borderBottomColor: Colors.accent500,
+        borderBottomWidth: 2,
+        color: Colors.accent500,
+        marginVertical: 8,
+        fontWeight: 'bold',
+        textAlign: 'center'
+
+    },
+    buttonsContainer: {
+        flexDirection: 'row'
+    },
+    buttonContainer:{
+        flex: 1
     }
+
+
 })
