7c94dc94bb60144d7b44332d457131fc6bccec27
Vijay Chendrayan Chandrasekar <vijaychendrayan@gmail.com>
2023-04-01 19:40:37 +0530

    Fix - Splash screen warning

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import { useCallback, useEffect, useState } from 'react';
-import { StyleSheet, ImageBackground, SafeAreaView} from 'react-native';
+import { StyleSheet, ImageBackground, SafeAreaView, View} from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
-import { useFonts } from 'expo-font';
+import * as Font from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen'
+import { StatusBar } from 'expo-status-bar';
 import AppLoading from 'expo-app-loading';
 import StartGameScreen from './screens/StartGameScreen';
 import GameScreen from './screens/GameScreen';
@@ -10,26 +11,77 @@ import Colors from './constants/colors';
 import GameOverScreen from './screens/GameOverScreen';
 
 
-
+// Keep the splash screen visible while we fetch resources
+// console.log("Visible Splash");
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
   const [userNumber, setUserNumber] = useState();
   const [gameIsOver, setGameIsOver] = useState(true);
   const [guessRounds, setGuessRounds] = useState(0)
   
+  //----------------- Splash Screen with Font Loading ---------------
+  const [appIsReady, setAppIsReady] = useState(false);
 
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // Pre-load fonts, make any API calls you need to do here
+        // console.log("Start Loading Fonts - try");
+        await Font.loadAsync(
+                      { 'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+                        'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+                      }
+                      
+        );
+        
+        // Artificially delay for two seconds to simulate a slow loading
+        // experience. Please remove this if you copy and paste the code!
+        // await new Promise(resolve => setTimeout(resolve, 2000));
+      } catch (e) {
+        // console.log("Error Loading Fonts");
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        // console.log("finally Loading Fonts");
+        setAppIsReady(true);
+        
+      }
+    }
 
-  
+    prepare();
+  }, []);
+  // console.log("App is Ready : ",appIsReady);
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      // This tells the splash screen to hide immediately! If we call this after
+      // `setAppIsReady`, then we may see a blank screen while the app is
+      // loading its initial state and rendering its first pixels. So instead,
+      // we hide the splash screen once we know the root view has already
+      // performed layout.
+      // console.log("Hide Splash");
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
 
-  const [fontsLoaded]=useFonts({
-    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
-    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
-  });
-
-  if(!fontsLoaded){
-    return <AppLoading />
-    
+  if (!appIsReady) {
+    // console.log("Return : Null");
+    return null;
   }
+ 
+  //----------------- End -------------------------------------------
+
+
+
+  // const [fontsLoaded]=useFonts({
+  //   'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+  //   'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+  // });
+
+  // if(!fontsLoaded){
+  //   return <AppLoading />
+    
+  // }
 
   function pickedNumberHandler(pickedNumber){
     setUserNumber(pickedNumber);
@@ -54,18 +106,21 @@ export default function App() {
   }
   
   return (
-    <LinearGradient colors={[Colors.primary700,Colors.accent500]} style={styles.rootScreen}>
-      <ImageBackground 
-        source={require('./assets/images/dice.jpg')}
-        resizeMode="cover" 
-        style={styles.rootScreen}
-        imageStyle={styles.backgroundImage}
-      >
-      <SafeAreaView style={styles.rootScreen}>
-        {screen}
-      </SafeAreaView>  
-      </ImageBackground>
-    </LinearGradient>    
+    <>
+      <StatusBar style='light'/>
+      <LinearGradient colors={[Colors.primary700,Colors.accent500]} style={styles.rootScreen} onLayout={onLayoutRootView}>
+          <ImageBackground 
+            source={require('./assets/images/dice.jpg')}
+            resizeMode="cover" 
+            style={styles.rootScreen}
+            imageStyle={styles.backgroundImage}
+          >
+          <SafeAreaView style={styles.rootScreen}>
+            {screen}
+          </SafeAreaView>  
+          </ImageBackground>
+      </LinearGradient>      
+    </>   
   );
 }
 
